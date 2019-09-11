0611d650abd26188703a4fe4daa6dd4e3571e1f4
conner <connersmith84@gmail.com>
2019-09-11 11:53:58 -0400

    Added abstraction to parsing cli args to a struct with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,20 @@
 use std::env;
-use std::fs;
 use std::process;
+mod minigrep_config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Here are the arguments, {:?}", &args);
 
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = minigrep_config::Config::new(&args).unwrap_or_else(|err|{
         println!("There was an error processing your args: {}", err);
         process::exit(1);
     });
 
-    let source_contents = fs::read_to_string(config.search_source_file)
-        .expect("Your file could not be read");
+    if let Err(e) = minigrep_config::run(config) {
+        println!("Error in reading text. Error: {}", e);
 
-
-}
-
-
-
-struct Config {
-    search_string: String,
-    search_source_file: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3{
-            return Err("There are not enough arguments");
-        }
-
-        let search_string = args[1].clone();
-        let search_source_file = args[2].clone();
-
-        Ok(Config{search_string,search_source_file})
+        process::exit(1);
     }
 }
+
---
+++ src/minigrep_config.rs
@@ -0,0 +1,58 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub search_string: String,
+    pub search_source_file: String,
+    }
+
+impl Config {
+     pub fn new(args: &[String]) -> Result<Config, &'static str>{
+         if args.len() < 3{
+             return Err("There are not enough arguments");
+         }
+         let search_string = args[1].clone();
+         let search_source_file = args[2].clone();
+         Ok(Config{search_string,search_source_file})
+     }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let source_contents = fs::read_to_string(config.search_source_file)
+        .expect("Your file could not be read");
+    let result = search(&config.search_string, &source_contents);
+    for line in result{
+        println!("{}", line)
+    }
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_test(){
+        let query = "spicy";
+        let contents = "Rust is honestly \nbig spicy";
+
+        assert_eq!(
+            vec!["big spicy"],
+            search(query, contents)
+        );
+    }
+}
+
+
